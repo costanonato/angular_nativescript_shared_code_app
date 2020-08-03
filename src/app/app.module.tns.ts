e10387f9ab7c9e6d05c774b447070b3579864336
Nonato <nonato.web@gmail.com>
2020-08-03 13:28:16 -0300

    base config to work with: 'ng8.2', 'ns6.7.0' and '@nativescript' packages

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,16 +1,16 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
-
+import { NativeScriptModule } from '@nativescript/angular';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
 
 
+
 // Uncomment and add to NgModule imports if you need to use two-way binding
-// import { NativeScriptFormsModule } from 'nativescript-angular/forms';
+// import { NativeScriptFormsModule } from '@nativescript/angular';
 
 // Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
-// import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
+// import { NativeScriptHttpClientModule } from '@nativescript/angular';
 
 @NgModule({
   declarations: [
